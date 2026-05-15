7302c358b1ad37d3a73048a6a47fe1362ace3740
hzy <1795438624@qq.com>
2026-05-15 14:26:23 +0800

    update

--- CHAPT3/topic_ws/src/demo_cpp_topic/src/turtle_control copy.cpp
+++ CHAPT3/topic_ws/src/demo_cpp_topic/src/turtle_control copy.cpp
@@ -3,41 +3,30 @@
 #include <chrono>
 #include "turtlesim/msg/pose.hpp"
 
-using namespace std::chrono_literals;       // 命名空间将参数自动转换为操作符
+using namespace std::chrono_literals;
 
-class TurtlrControlNode: public rclcpp::Node     // 继承来自rclcpp下的Node节点
+class TurtlrControlNode: public rclcpp::Node
 {
     private:
-    /* 声明变量 */
-        // 先创建共享指针，不赋值
-        // rclcpp::TimerBase::SharedPtr timer_;    // 定时器指针
-        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;     // 发布者的智能共享指针
-        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber_;      // 订阅者的智能共享指针
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber_;
         double target_x_{1.0};
         double target_y_{1.0};
-        double k_{1.0};             // 比例系数
-        double max_speed_{3.0};     // 最大速度
-        // double max_ang_{};
+        double k_{1.0};
+        double max_speed_{3.0};
     public:
         explicit TurtlrControlNode(const std::string& node_name):Node(node_name)
         {
-            // 共享指针赋值
             publisher_ = this-> create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
             subscriber_ = this -> create_subscription<turtlesim::msg::Pose>("/turtle1/pose"
                             , 10, std::bind(&TurtlrControlNode::on_pose_received_, this, std::placeholders::_1));
-            // timer_ = this -> create_wall_timer(1000ms, std::bind(&TurtlrControlNode::timer_callback, this));
-            // this : 指向当前对象的指针
         }
-        
-        // 成员函数, 参数：收到数据的共享指针
         void on_pose_received_(const turtlesim::msg::Pose::SharedPtr pose)
         {
-            // 获取当前位置
             auto current_x = pose -> x;
             auto current_y = pose -> y;
             RCLCPP_INFO(get_logger(), "now:x=%f, y=%f", current_x, current_y);
 
-            // 计算当前海龟位置与目标位置差
             auto distance = std::sqrt(
                 (target_x_ - current_x) * (target_x_ - current_x)+
                 (target_y_ - current_y) * (target_y_ - current_y)
@@ -46,7 +35,6 @@ class TurtlrControlNode: public rclcpp::Node     // 继承来自rclcpp下的Node
                 (target_y_ - current_y), (target_x_ - current_x)
             ) - pose->theta;
 
-            // 控制策略
             auto msg = geometry_msgs::msg::Twist();
             if(distance > 0.1)
             {
@@ -60,13 +48,11 @@ class TurtlrControlNode: public rclcpp::Node     // 继承来自rclcpp下的Node
                 }
             }
 
-            // 限制线速度最大值
             if(msg.linear.x > max_speed_)
             {
                 msg.linear.x = max_speed_;
             }
             
-            // 发布话题
             publisher_->publish(msg);
         }
 };
